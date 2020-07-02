67d22302175a1458b6400beea513341cd221c75b
Nabaraj Saha <nabara2k@gmail.com>
2020-07-02 15:00:40 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Node express serverless project
+api is availa
+
+## To Start Project
+```
+npm install
+npm start //to start local version
+npm deploy //to deploy in aws but need to configure local aws
+```
+
+api path is available [here](https://o7qm3lumra.execute-api.us-east-1.amazonaws.com/production )
+
+### Support Calls
+| Name | Method | Path |
+| ------ | ------ | ------ |
+| base path | GET |/|
+| show time | GET |/time|
+| log text | POST |/logthis|
---
+++ src/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+
+//GET
+app.get('/', (req, res) => {
+    res.status(200).send('hello world!');
+});
+
+//GET req Simply sends the current time
+app.get('/time', (req, res) => {
+    let timeNow = Date(Date.now());
+    res.status(200).send(timeNow.toString());
+});
+
+//POST req logs the name and sends it
+//To check send a POST req with "name" and check your lambda function console
+app.post('/logthis', (req, res) => {
+    const name = req.body.name;
+    const toLog = `\n >>> My name is ${name} <<< \n`
+    console.info(toLog)
+    res.status(200).send(toLog);
+});
+
+module.exports = app;
